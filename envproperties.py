c7278784da04d6eadeba320812b8501a3ed3847e
Anthony <anthony.stonaker@gmail.com>
2015-04-28 19:05:40 -0600

    properties.py and .gitignore
    Enhancing properties files to be able to separate gnucash and member DBs

--- envproperties.py
+++ envproperties.py
@@ -1,5 +1,14 @@
-gnucdbstring	= 'mysql://user:password@127.0.0.1/startdb'
-dbserver 		= '127.0.0.1'
-dbuser 			= 'user'
-dbpassword 		= 'password'
-dbschema		= 'startdb'
+# These properties are used by DenhacGncLibrary.DenhacGncSession
+gnucdbstring		= 'mysql://user:password@127.0.0.1/startdb'
+
+# These properties are loaded by DenhacDbLibrary.DenhacGnucashDb
+gnc_db_server 		= '127.0.0.1'
+gnc_db_user 		= 'user'
+gnc_db_password 	= 'password'
+gnc_db_schema		= 'gnucashdb'
+
+# These properties are loaded by DenhacDbLibrary.DenhacMemberDb
+member_db_server 	= '127.0.0.1'
+member_db_user 		= 'user'
+member_db_password 	= 'password'
+member_db_schema	= 'memberdb'
