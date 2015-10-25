1f2f89f11ee7880b09e0083bb6f74db5dbe11e55
Anthony <anthony.stonaker@gmail.com>
2015-10-25 15:10:09 -0600

    Working on /login function; fixed include path error.

--- server/apifunctions.py
+++ server/apifunctions.py
@@ -1,17 +1,15 @@
 import sys
-sys.path.insert(0, '../denhacpkg')
+sys.path.insert(0, '/var/www/denhacpkg')
+sys.path.insert(0, '/var/www/server')
 
-from flask import Flask, request, session
+from flask import Flask, request, session, flash, abort
 from DenhacLdapLibrary import DenhacLdapLibrary
 
-
-
-# This is only invoked when run from the command-line from testing:
+###### This is only invoked when run from the command-line from testing:
 app = Flask(__name__)
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080)
-
-
+    app.run(host='127.0.0.1', port=8080, debug=True)
+######
 
 
 @app.route("/hello")
@@ -19,26 +17,56 @@ def hello():
     return "Goodbye, cruel world."
 
 
+@app.route("/abort")
+def abort():
+	abort(401)
+
+@app.route("/flash")
+def flash():
+	flash("I am the Flash!")
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	user     = None
+	password = None
+
+	try:
+		if request.method == 'POST':
+			user   = request.form['user']
+			password = request.form['password']
+		elif request.method == 'GET':
+			user   = request.args.get("user")
+			password = request.args.get("password")
+#
+#		if not user:
+#			return "User is required!"
+#		if not pass:
+#			return "Pass is required!"
+#
+
+	except KeyError:
+		return "User and Password are required!"
+
+
+	return "I am here."
+
 	try:
 		myLdap = DenhacLdapLibrary()
-		myLdap.ldapBind(request.form['user'], request.form['pass'])
+		myLdap.ldapBind(request.form['user'], request.form['password'])
 
 		session['logged_in'] = True
 		flash('You were logged in')
 #        return redirect(url_for('show_entries'))
 
 	except:
-		return render_template('login.html', error=error)
+		return "Some exception somewhere!"
 
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
-
+#@app.route('/logout')
+#def logout():
+#    session.pop('logged_in', None)
+#    flash('You were logged out')
+#    return redirect(url_for('show_entries'))
+#
