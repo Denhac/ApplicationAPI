9c8df63bd529c9e33463ced3e40b1583fc8a1559
Anthony <anthony.stonaker@gmail.com>
2015-10-25 14:25:38 -0600

    Committing first [working] draft of loading an API via Apache (WSGI mod), and invoked remotely via a browser. See /login. Need it to speak JSON now and actually interface to LDAP for /login test.

---
+++ denhacpkg/apifunctions.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/denhacpkg')
+sys.path.insert(0, '/var/www/server')
+from apifunctions import app as application
--- server/apifunctions.py
+++ server/apifunctions.py
@@ -1,13 +1,16 @@
 import sys
+sys.path.insert(0, '../denhacpkg')
 
-from flask import Flask
+from flask import Flask, request, session
+from DenhacLdapLibrary import DenhacLdapLibrary
+
+
+
+# This is only invoked when run from the command-line from testing:
 app = Flask(__name__)
-
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080)
 
-
-sys.path.insert(0, '../denhacpkg')
 
 
 
@@ -21,17 +24,17 @@ def hello():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+	try:
+		myLdap = DenhacLdapLibrary()
+		myLdap.ldapBind(request.form['user'], request.form['pass'])
+
+		session['logged_in'] = True
+		flash('You were logged in')
+#        return redirect(url_for('show_entries'))
+
+	except:
+		return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
@@ -39,17 +42,3 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('show_entries'))
 
-
-
-try:
-	myLdap = DenhacLdapLibrary()
-	myLdap.ldapBind(user, myPass)
-	print "SUCCESS!"
-
-
-except:
-	import traceback
-	print "Whoops, you fail."
-	print '-'*60
-	traceback.print_exc(file=sys.stdout)
-	print '-'*60
