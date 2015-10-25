a0ce0927f0c4d4fd999482e3b2fe6bb353a182ea
Anthony <anthony.stonaker@gmail.com>
2015-10-25 13:17:54 -0600

    Fixed typo; testing credentials store

--- test-call-json-service.py
+++ test-call-json-service.py
@@ -3,7 +3,7 @@
 import json
 import urllib2
 
-# Load our envrionment config file
+# Load our environment config file
 myprops = dict(line.strip().split('=') for line in open('./denhac.properties'))
 host = myprops['api_server']
 print host
