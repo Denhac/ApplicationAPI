9088d8b1fa780d88a62833bd66e9b0ba198b2800
Anthony <anthony.stonaker@gmail.com>
2017-03-23 19:31:26 -0600

    Added proper docstrings

--- server/apifunctions.py
+++ server/apifunctions.py
@@ -37,7 +37,14 @@ if app.debug is not True:
 ## Global function to check if a user is logged in and has permissions
 @app.before_request
 def before_request():
-	admin_services   = ['hello']
+	"""
+		Check for an active session and for proper permissions.
+
+		This function is implicitly called at the beginning of every app.route call below.
+		It returns http 500 with a permissions error if the user does not have the proper
+		level of access.
+	"""
+	admin_services   = []
 	manager_services = ['memberpaymentreport', 'createMember', 'readMembers', 'readMember', 'createpayment', 'searchmember', 'getopenbalances', 'getmember', 'getmemberbalance', 'importpaypaldata', 'createmember', 'editmember']
 
 	# Ensure member is logged in to access any APIs
@@ -57,12 +64,14 @@ def before_request():
 # Global error handler for Internal Server Errors
 @app.errorhandler(500)
 def internal_error(exception):
+	"""Catch any unhandled error and reply with http 500"""
 	app.logger.error(exception)
 	return DenhacJsonLibrary.ReplyWithError("Internal Server Error")
 
 # Global error handler - any uncaught exceptions will go here.
 @app.errorhandler(Exception)
 def exception_handler(error):
+	"""Catch any unhandled exception and reply with http 500"""
 	app.logger.error(error)
 	return DenhacJsonLibrary.ReplyWithError("ERROR: " + repr(error))
 ####################################################################################
@@ -70,6 +79,7 @@ def exception_handler(error):
 # Main page - login form to enter user/pw
 @app.route('/')
 def main():
+	"""Display the login form to the user."""
 #	return render_template('login.html')
 # TODO - WHY THE @#$%^ DO TEMPLATES WORK ON THE DEV API SERVER AND NOT THE PROD API SERVER???
 	return '''
@@ -90,11 +100,15 @@ def main():
 # Hello world tester (You still can't access this if you aren't logged in though - see @app.before_request)
 @app.route("/hello")
 def hello():
+	"""Simple hello world test service."""
 	return DenhacJsonLibrary.ObjToJson(dict(msg = "Goodbye, cruel world."))
 
 ####################################################################################
 
 def antiBruteForceLogic():
+	"""
+	Attempt to detect brute-force guessing of passwords, and force the user to wait.
+	"""
 	# Force a wait every few consecutive login failures
 	session.pop('logged_in', None)
 	if 'login_tries' not in session:
@@ -110,6 +124,16 @@ def antiBruteForceLogic():
 # Login method - POSTed to here by index.html; could be called from anywhere
 @app.route('/login', methods=['POST'])
 def login():
+	"""
+	Login a user against the configured LDAP server, and save session data if valid.
+
+	Access Level:		All
+	Accepted methods:	POST
+
+	Inputs:		username
+				password
+	Returns:	{"logged_in": "True", "username": <username>} or error
+	"""
 	username = None
 	password = None
 
@@ -149,6 +173,7 @@ def login():
 # Logout method
 @app.route('/logout')
 def logout():
+	"""Logout the requested user."""
 	session.pop('logged_in', None)
 	return DenhacJsonLibrary.ObjToJson(dict(logged_out = "True"))
 
@@ -167,6 +192,18 @@ def logout():
 
 @app.route('/createpayment', methods=['GET'])
 def createpayment():
+	"""
+	Create a payment against a specific member's account.
+
+	Access Level:		Manager
+	Accepted methods:	GET
+
+	Inputs:		member_id			The member ID in the DB
+				amount				The amount to credit(+) or debit(-)
+				payment_type_id		Links to an ID in the payment_type table
+				notes				The memo field for the transaction
+	Returns:	{"created": "True"} or error
+	"""
 	try:
 		# Will throw KeyError if vars not present
 		member_id       = request.args['member_id']
@@ -187,6 +224,20 @@ def createpayment():
 
 @app.route('/searchmember', methods=['GET'])
 def searchmember():
+	"""
+	Search for a member in the DB.
+
+	The service searches multiple fields, including first name, last name, and handle.
+	Return any matching member in an array.
+
+	Accepted methods: GET
+
+	Inputs:		search_str			The string to search
+
+	Returns: {"rows": [{"firstName": "string", "string": "DFM001", "lastName": "string", "paymentAmount": float, "middleInitial": "string", "contact_email": "string", "onAutoPay": bool, "active": bool, "paypal_email": "string", "id": int}]}
+			or error
+	"""
+
 	try:
 		# Will throw KeyError if vars not present
 		search_str = request.args['search_str']
@@ -204,12 +255,23 @@ def searchmember():
 
 @app.route('/getpaymenttypes')
 def getpaymenttypes():
+	"""Return a list of valid payment types from the DB."""
 	memberDb = DenhacMemberDb()
 	resultList = memberDb.getPaymentTypes()
 	return DenhacJsonLibrary.ObjToJson(dict(rows = resultList))
 
 @app.route('/getopenbalances')
 def getopenbalances():
+	"""
+	Return an array of members and the balance owed for each.
+
+	Access Level:		Manager
+	Accepted methods:	GET
+
+	Inputs:		none
+	Returns:	{"rows": [{"businessNumber": "string", "prox_card_id": "string", "ad_username": "string", "driver_license": "string", "paypal_email": "string", "id": int, "city": "string", "paymentAmount": float, "isAdmin": int, "emerAddress2": "string", "emerAddress1": "string", "phoneNumber": "string", "onAutoPay": int, "emerRelation1": "string", "emerRelation2": "string", "streetAddress2": "string", "streetAddress1": "string", "isManager": int, "zipCode": "string", "contact_email": , "active": int, "emerPhone1": "string", "emerPhone2": "string", "firstName": "string", "gnuCashId": "string", "lastName": "string", "middleInitial": "string", "birthdate": "string", "join_date": "string", "emerContact2": "string", "emerContact1": "string", "balance": float, "medicalConditionList": "string"}]}
+				or error
+	"""
 	memberDb = DenhacMemberDb()
 	resultList = memberDb.getOpenBalances()
 	return DenhacJsonLibrary.ObjToJson(dict(rows = resultList))
@@ -217,6 +279,16 @@ def getopenbalances():
 @app.route('/getmember', defaults={'member_id':'-1'})
 @app.route('/getmember/<member_id>')
 def getmember(member_id):
+	"""
+	Return a single member row by ID.
+
+	Access Level:		Manager
+	Accepted methods:	GET
+
+	Inputs:		member_id
+	Returns:	{"row": {"businessNumber": "string", "prox_card_id": "string", "ad_username": "string", "driver_license": "string", "paypal_email": "string", "id": int, "city": "string", "paymentAmount": float, "isAdmin": int, "emerAddress2": "string", "emerAddress1": "string", "phoneNumber": "string", "onAutoPay": int, "emerRelation1": "string", "emerRelation2": "string", "streetAddress2": "string", "streetAddress1": "string", "isManager": int, "zipCode": "string", "contact_email": , "active": int, "emerPhone1": "string", "emerPhone2": "string", "firstName": "string", "gnuCashId": "string", "lastName": "string", "middleInitial": "string", "birthdate": "string", "join_date": "string", "emerContact2": "string", "emerContact1": "string", "balance": float, "medicalConditionList": "string"}}
+				or error
+	"""
 	memberDb = DenhacMemberDb()
 	member   = memberDb.getMember(member_id)
 
@@ -228,6 +300,16 @@ def getmember(member_id):
 @app.route('/getmemberbalance', defaults={'member_id':'-1'})
 @app.route('/getmemberbalance/<member_id>')
 def getmemberbalance(member_id):
+	"""
+	Return a single member balance by ID with all transaction rows
+
+	Access Level:		Manager
+	Accepted methods:	GET
+
+	Inputs:		member_id
+	Returns:	{"balance": float, "rows": [{"amount": float, "transaction_date": "string", "type": "string", "notes": "string"}]}
+				or error
+	"""
 	memberDb = DenhacMemberDb()
 	resultList = memberDb.getMemberBalance(member_id)
 
@@ -239,7 +321,7 @@ def getmemberbalance(member_id):
 
 @app.route('/importpaypaldata', methods=['POST'])
 def importpaypaldata():
-
+	"""DEPRECATING SOON.  DO NOT USE."""
 	try:
 		# If post, they're sending us the file.
 		if request.method == 'POST':
@@ -310,6 +392,11 @@ def importpaypaldata():
 		return DenhacJsonLibrary.ReplyWithError("ERROR: Something failed for some reason.  Check the server logs for more details. Error: " + sys.exc_info()[0] + sys.exc_info()[1] + sys.exc_info()[2])
 
 def setFieldsArray():
+	"""
+	Read POSTed form data and return a cleaned-up dict() of the fields that actually have data.
+
+	This function is called from both the createmember and editmember services.
+	"""
 	fields = dict()
 
 	# Required fields
@@ -378,6 +465,16 @@ def setFieldsArray():
 
 @app.route('/createmember', methods=['POST'])
 def createmember():
+	"""
+	Create a new member in the DB.
+
+	Access Level:		Manager
+	Accepted methods:	POST
+
+	Inputs:		***See the function setFieldsArray() above***
+	Returns:	{"success": "True"}
+				or error
+	"""
 	fields = setFieldsArray()
 
 	memberDb = DenhacMemberDb()
@@ -387,12 +484,19 @@ def createmember():
 
 @app.route('/editmember/<member_id>', methods=['POST'])
 def editmember(member_id):
+	"""
+	Edit a member in the DB.
+
+	Access Level:		Manager
+	Accepted methods:	POST
+
+	Inputs:		***See the function setFieldsArray() above***
+	Returns:	{"success": "True"}
+				or error
+	"""
 	fields = setFieldsArray()
 
 	memberDb = DenhacMemberDb()
 	memberDb.editMember(member_id, fields)
 
 	return DenhacJsonLibrary.ObjToJson(dict(success = True))
-
-
-
